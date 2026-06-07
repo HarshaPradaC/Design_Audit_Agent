9e2d599aa42462db9f364d15caf2cd9db71c997e
HarshaPradaC <harshapradac@gmail.com>
2026-06-08 04:11:19 +0530

    frontend/config: Vite, Tailwind, PostCSS, and npm package setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: { 900: '#0f1117', 800: '#1e293b', 700: '#334155', 600: '#475569' }
+      }
+    }
+  },
+  plugins: []
+}
